488c9c0fec5ad0b157ce9337c9e1aec00a13c687
adamGuan <ghq1986@gmail.com>
2014-12-30 19:12:46 +0800

    some2

--- sql_patch/dream.sql
+++ sql_patch/dream.sql
@@ -16,21 +16,32 @@ CREATE DATABASE IF NOT EXISTS `dream_fudao_admin` /*!40100 DEFAULT CHARACTER SET
 USE `dream_fudao_admin`;
 
 
--- 导出  表 dream_fudao_admin.t_role 结构
-DROP TABLE IF EXISTS `t_role`;
-CREATE TABLE IF NOT EXISTS `t_role` (
+-- 导出  表 dream_fudao_admin.t_privity_group 结构
+DROP TABLE IF EXISTS `t_privity_group`;
+CREATE TABLE IF NOT EXISTS `t_privity_group` (
   `F_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `F_name` varchar(50) NOT NULL COMMENT '角色名',
-  `F_description` text NOT NULL COMMENT '角色的描述',
+  `F_pid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '父组ID',
+  `F_name` varchar(50) NOT NULL COMMENT '组名',
+  `F_description` text NOT NULL COMMENT '组的描述',
   `F_privity` text NOT NULL COMMENT '权限(例:  1,15,6)',
-  `F_status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态(1:有效,0无效)',
+  `F_status` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '状态(1:有效,0无效)',
+  `F_could_has_child` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '组下面是否可以创建子组(1:可以,0不 可以)',
+  `F_create_by_uid` int(10) unsigned NOT NULL COMMENT 't_user对应的F_id',
+  `F_create_time` datetime NOT NULL COMMENT '创建时间',
+  `F_modify_time` datetime NOT NULL COMMENT '修改时间',
+  `F_level` tinyint(1) NOT NULL DEFAULT '-1' COMMENT '0为超级管理员，-1为其它,1为老师',
   PRIMARY KEY (`F_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='角色表';
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='权限组';
 
--- 正在导出表  dream_fudao_admin.t_role 的数据：0 rows
-DELETE FROM `t_role`;
-/*!40000 ALTER TABLE `t_role` DISABLE KEYS */;
-/*!40000 ALTER TABLE `t_role` ENABLE KEYS */;
+-- 正在导出表  dream_fudao_admin.t_privity_group 的数据：4 rows
+DELETE FROM `t_privity_group`;
+/*!40000 ALTER TABLE `t_privity_group` DISABLE KEYS */;
+INSERT INTO `t_privity_group` (`F_id`, `F_pid`, `F_name`, `F_description`, `F_privity`, `F_status`, `F_could_has_child`, `F_create_by_uid`, `F_create_time`, `F_modify_time`, `F_level`) VALUES
+	(1, 0, '超级管理员', '', 'all', 1, 1, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0),
+	(2, 1, '运营管理者', '', '', 1, 1, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', -1),
+	(3, 2, '运营人员', '', '', 1, 0, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', -1),
+	(4, 1, '测试组', '', '', 1, 0, 1, '0000-00-00 00:00:00', '2014-12-30 15:12:43', -1);
+/*!40000 ALTER TABLE `t_privity_group` ENABLE KEYS */;
 
 
 -- 导出  表 dream_fudao_admin.t_teacher 结构
@@ -54,15 +65,17 @@ CREATE TABLE IF NOT EXISTS `t_user` (
   `F_login_name` varchar(50) NOT NULL COMMENT '登录名',
   `F_login_password` char(32) NOT NULL COMMENT '登录密码',
   `F_nick_name` varchar(50) NOT NULL COMMENT '昵称',
-  `F_role_id` int(10) NOT NULL COMMENT '角色ID',
+  `F_privity_group_id` int(10) NOT NULL COMMENT '权限组ID',
   PRIMARY KEY (`F_id`),
   UNIQUE KEY `F_login_name` (`F_login_name`),
-  KEY `F_role_id` (`F_role_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='后台用户表';
+  KEY `F_role_id` (`F_privity_group_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='后台用户表';
 
--- 正在导出表  dream_fudao_admin.t_user 的数据：0 rows
+-- 正在导出表  dream_fudao_admin.t_user 的数据：1 rows
 DELETE FROM `t_user`;
 /*!40000 ALTER TABLE `t_user` DISABLE KEYS */;
+INSERT INTO `t_user` (`F_id`, `F_login_name`, `F_login_password`, `F_nick_name`, `F_privity_group_id`) VALUES
+	(1, 'admin', 'admin', 'admin', 1);
 /*!40000 ALTER TABLE `t_user` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
